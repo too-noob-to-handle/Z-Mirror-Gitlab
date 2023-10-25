c031292eb54d7ffeec350c9c4d86a1a5c295c4bc
too-noob-to-handle <98410308+too-noob-to-handle@users.noreply.github.com>
2023-10-25 16:38:38 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -306,7 +306,7 @@ if len(BASE_URL) == 0:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://gitlab.com/Dawn-India/Z-Mirror'
+    UPSTREAM_REPO = 'https://github.com/too-noob-to-handle/Z-Mirror-Gitlab'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
